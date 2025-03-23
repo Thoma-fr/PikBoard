06af47582c303eae198e0e0c834077ce81c49a12
Cristian URSU <cristian.ursu.2001@gmail.com>
2025-03-22 20:39:53 -0400

    setup navigation

---
+++ app/src/main/java/com/example/pikboard/ui/screens/LoginScreen.kt
@@ -0,0 +1,16 @@
+package com.example.pikboard.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LoginScreen() {
+    Text("This is the login screen")
+}
+
+@Preview
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen()
+}
---
+++ app/src/main/java/com/example/pikboard/ui/screens/Routes.kt
@@ -0,0 +1,6 @@
+package com.example.pikboard.ui.screens
+
+object Routes {
+    const val LOGIN_PAGE = "LOGIN_PAGE"
+    const val SIGNUP_PAGE = "SIGUP_PAGE"
+}
