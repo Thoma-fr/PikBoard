3b76d014e0ba27f10820373716a94e0b3d99671e
Cristian URSU <cristian.ursu.2001@gmail.com>
2025-03-24 12:17:01 -0400

    setup navbar

--- app/src/main/java/com/example/pikboard/MainActivity.kt
+++ app/src/main/java/com/example/pikboard/MainActivity.kt
@@ -6,12 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.pikboard.ui.Fragment.PikNavBar
 import com.example.pikboard.ui.screens.HomeScreen
 import com.example.pikboard.ui.screens.auth.LoginScreen
 import com.example.pikboard.ui.screens.Routes
@@ -35,20 +39,33 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun PikBoardApp(innerPadding: PaddingValues) {
         val navController = rememberNavController()
-        NavHost(navController = navController, startDestination = Routes.Auth.LOGIN_PAGE){
-            composable(Routes.Auth.LOGIN_PAGE) {
-                LoginScreen(innerPadding, navController)
-            }
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        Scaffold(
+            bottomBar = {
+                if (currentRoute != Routes.Auth.LOGIN_PAGE &&
+                    currentRoute != Routes.Auth.SIGNUP_PAGE) {
+                    PikNavBar()
+                }
 
-            composable(Routes.Auth.SIGNUP_PAGE) {
-                SignupScreen(innerPadding, navController)
             }
-
-            composable(Routes.HOME_PAGE) {
-                HomeScreen(innerPadding, navController)
+        ) { scaffoldPadding ->
+            NavHost(
+                navController = navController,
+                startDestination = Routes.Auth.LOGIN_PAGE,
+                modifier = Modifier.padding(scaffoldPadding)
+            ) {
+                composable(Routes.Auth.LOGIN_PAGE) {
+                    LoginScreen(navController)
+                }
+                composable(Routes.Auth.SIGNUP_PAGE) {
+                    SignupScreen(navController)
+                }
+                composable(Routes.HOME_PAGE) {
+                    HomeScreen(navController)
+                }
             }
         }
     }
-
 }
 
--- app/src/main/java/com/example/pikboard/ui/Fragment/DisplayFramgment.kt
+++ app/src/main/java/com/example/pikboard/ui/Fragment/DisplayFramgment.kt
@@ -1,9 +1,31 @@
 package com.example.pikboard.ui.Fragment
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -23,4 +45,83 @@ fun PikHeader(modifier: Modifier = Modifier) {
 @Composable
 fun PikHeaderPreview() {
     PikHeader()
+}
+
+data class BottomNavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val hasNews: Boolean,
+    val badgeCount: Int? = null
+)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun PikNavBar() {
+    val items = listOf(
+        BottomNavigationItem(
+            title = "Home",
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home,
+            hasNews = false,
+        ),
+        BottomNavigationItem(
+            title = "Plus",
+            selectedIcon = Icons.Filled.Add,
+            unselectedIcon = Icons.Outlined.Add,
+            hasNews = false,
+        ),
+        // TODO: Ajouter plus d'icone (pour pouvoir avoir un icon "groupe")
+        BottomNavigationItem(
+            title = "Friend",
+            selectedIcon = Icons.Filled.Share,
+            unselectedIcon = Icons.Outlined.Share,
+            hasNews = false,
+        ),
+        BottomNavigationItem(
+            title = "Profile",
+            selectedIcon = Icons.Filled.Person,
+            unselectedIcon = Icons.Outlined.Person,
+            hasNews = false,
+        ),
+    )
+    var selectedItemIndex by rememberSaveable { mutableStateOf(0) }
+
+    NavigationBar() {
+        items.forEachIndexed{ index, item ->
+            NavigationBarItem(
+                selected = selectedItemIndex == index,
+                onClick = {
+                    selectedItemIndex = index
+                    // TODO: Add the navigation here
+                          },
+                label = {
+                    Text(text = item.title)
+                },
+                alwaysShowLabel = false,
+                icon = {
+                    BadgedBox(badge = {
+                        if (item.badgeCount != null) {
+                            Badge {
+                                Text(text= item.badgeCount.toString())
+                            }
+                        } else if (item.hasNews) {
+                            Badge()
+                        }
+                    }) {
+                        Icon(imageVector = if(index==selectedItemIndex) {
+                            item.selectedIcon
+                        } else item.unselectedIcon,
+                            contentDescription = item.title)
+                    }
+
+                },
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PikNavBarPreview() {
+    PikNavBar()
 }
